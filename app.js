c0e9cc4d6200f467ecaae0fe1ed2e36730ba5c37
mackawara <84657473+mackawara@users.noreply.github.com>
2022-05-28 19:10:50 +0200

    added bcrpt and passport libraries

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
-const figlet=require("figlet")
-const http = require('http');
+const figlet = require("figlet");
+const http = require("http");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const passportLocal = require("passport-local");
 
-const hostname = '127.0.0.1';
+
+
+async function hashFunction(hashTarget, salt) {
+  const hashedTarget = await bcrypt.hash(hashTarget, salt);
+  console.log(hashedTarget);
+}
+console.time("hashing");
+hashFunction(name, 10);
+console.timeEnd("hashing");
+
+console.time("hashing2");
+hashFunction(name, 20);
+console.timeEnd("hashing2");
+
+async function compare() {
+  bcrypt.compare(name, hashedTarget);
+}
+
+const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
