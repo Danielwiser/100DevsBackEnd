f4311ddc9718a4d300a4a1d6e95aaf09f7b266e0
mackawara <84657473+mackawara@users.noreply.github.com>
2022-05-29 08:26:29 +0200

    added dependencies passport bcrytp

--- app.js
+++ app.js
@@ -4,14 +4,39 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const passport = require("passport");
 const passportLocal = require("passport-local");
+const fs = require('fs');
 
 
+
+const hostname = "127.0.0.1";
+const port = 3000;
+/* create Server usinf HTTP  */
+const data= {name:"Mac"}
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.writeHead(200, { "Content-Type": "text/html" });
+
+  var url = req.url;
+
+  if (url === "/") {
+    res.write(data.name)
+    res.end();
+  } else if (url === "/contact") {
+    res.write(" Welcome to contact us page");
+    res.end();
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 async function hashFunction(hashTarget, salt) {
   const hashedTarget = await bcrypt.hash(hashTarget, salt);
   console.log(hashedTarget);
 }
-console.time("hashing");
+/* console.time("hashing");
 hashFunction(name, 10);
 console.timeEnd("hashing");
 
@@ -19,19 +44,6 @@ console.time("hashing2");
 hashFunction(name, 20);
 console.timeEnd("hashing2");
 
-async function compare() {
+ */ async function compare() {
   bcrypt.compare(name, hashedTarget);
 }
-
-const hostname = "127.0.0.1";
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
