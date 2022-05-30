246bc2b4376ca82f5efcc9d1a0a30e5d61b53958
mackawara <84657473+mackawara@users.noreply.github.com>
2022-05-30 07:00:57 +0200

    restore point before removing express

--- app.js
+++ app.js
@@ -4,14 +4,34 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const passport = require("passport");
 const passportLocal = require("passport-local");
-const fs = require('fs');
+const fs = require("fs");
 
-const express=requie("express")
-const app=express()
+const bodyparser = require("body-parser");
+//const urlEncoded = require("url");
 
+const express = require("express");
+const app = express();
+
+/* middleware */
+app.use(body-parser);
+//app.use(ur-lEncoded);
+app.use(express.static(__dirname + "/public"));
+const multer = require("multer");
+app.use(express.json());
+app.use(urlencoded)
+const port = 3000;
+app.listen(port, () => {
+  console.log("server running");
+});
+
+/* Routes */
+
+app.get("/", (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200).send(req.body);
+});
 
 const hostname = "127.0.0.1";
-const port = 3000;
 
 async function hashFunction(hashTarget, salt) {
   const hashedTarget = await bcrypt.hash(hashTarget, salt);
